3c48ccb8bffb7fac36551e2451ea6a100eb0b1ea
mistlight <mistlight@otakukingdom.com>
2016-11-19 16:18:14 -0500

    initial commit

---
+++ src/main/java/com/otakukingdom/audiobook/Main.java
@@ -0,0 +1,31 @@
+/**
+ * Created by mistlight on 11/19/2016.
+ */
+
+package com.otakukingdom.audiobook;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MainWindow.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
