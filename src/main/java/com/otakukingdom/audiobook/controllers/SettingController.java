4d90b52e509d8d279b6d793be29bfa137f05d4b8
mistlight <mistlight@otakukingdom.com>
2016-11-20 11:45:07 -0500

    laying out directory pane for settings window

--- src/main/java/com/otakukingdom/audiobook/controllers/SettingController.java
+++ src/main/java/com/otakukingdom/audiobook/controllers/SettingController.java
@@ -1,8 +1,31 @@
 package com.otakukingdom.audiobook.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
+
 /**
  * Created by mistlight on 11/20/2016.
  */
 public class SettingController {
 
+    @FXML
+    private TabPane rootPane;
+
+    @FXML
+    public void handleAddDirectoryAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void handleRemoveDirectoryAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void handleSaveAction(ActionEvent event) {
+        Stage currentSage = (Stage) rootPane.getScene().getWindow();
+        currentSage.close();
+    }
 }
